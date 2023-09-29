ac9a594ced14bbdd591722de013c63c50b359383
kipmaiyo <danielkipmaiyo44@gmail.com>
2023-09-29 17:26:49 +0300

    assignment done

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,11 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
+    def learn(self, knowledge_string):
+        self.knowledge.append(knowledge_string)
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
         pass
